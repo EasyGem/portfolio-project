3957648fe2a5ed821eea041322de70f1fbd7769c
EasyGem <san4es021@gmail.com>
2018-11-27 13:49:32 +0300

    44

--- api/send-email.js
+++ api/send-email.js
@@ -10,16 +10,11 @@ app.use(express.urlencoded({
 
 const sendMail = (name, email, message) => {
 	var transporter = nodemailer.createTransport({
-		service: 'Gmail',
-		auth: {
-			user: 'san4es021@gmail.com',
-			pass: 'gLa33e3F2iled'
-		}
-		// service: 'Hotmail',
-    // auth: {
-    //     user: "san4es_ag@outlook.com",
-    //     pass: "2wfAHlvxi33KLp2Z3abL"
-    // }
+		// service: 'Gmail',
+		// auth: {
+		// 	user: 'san4es021@gmail.com',
+		// 	pass: 'gLa33e3F2iled'
+		// }
 		// host: 'smtp.gmail.com',
     // port: 465,
     // secure: true,
@@ -27,13 +22,13 @@ const sendMail = (name, email, message) => {
 		// 	user: 'san4es021@gmail.com',
 		// 	pass: 'gLa33e3F2iled'
 		// }
-		// host: 'smtp.yandex.ru',
-		// port: 465,
-		// secure: true,
-		// auth: {
-		// 	user: 'YTAcademy@yandex.ru',
-		// 	pass: 'agusev_pass_yta'
-		// }
+		host: 'smtp.yandex.ru',
+		port: 465,
+		secure: true,
+		auth: {
+			user: 'YTAcademy@yandex.ru',
+			pass: 'agusev_pass_yta'
+		}
 	})
 	transporter.sendMail({
 		from: 'ytacademy@ya.ru',
