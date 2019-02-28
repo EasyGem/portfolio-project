e8dbf1006c54b333de0c0d4749cd8260a49919e8
Alexander Gusev <san4es021@gmail.com>
2019-02-28 19:52:30 +0300

    Mail

--- api/send-email.js
+++ api/send-email.js
@@ -1,47 +1,52 @@
-import express from 'express'
-import nodemailer from 'nodemailer'
+import express from "express"
+import nodemailer from "nodemailer"
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({
-	extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
 
 const sendMail = (name, email, message) => {
-	var transporter = nodemailer.createTransport({
-		host: 'smtp.yandex.ru',
-		port: 587,
-		auth: {
-			user: 'the.alex.dev@ya.ru',
-			pass: 's3IyASZAlgXBV5Y5GyZB'
-		}
-	})
-	transporter.sendMail({
-		from: 'the.alex.dev@ya.ru',
-		to: 'the.alex.dev@ya.ru',
-		subject: 'Заявка с Alex-Card.ru - Freelance',
-		html: `Имя: ${name} <br> Email: ${email} <br> Сообщение: ${message}`,
-	}, (error, info) => {
-		if (error) {
-			return console.log(error);
-		}
-		console.log(info);
-	})
+  var transporter = nodemailer.createTransport({
+    host: "smtp.yandex.ru",
+    port: 587,
+    auth: {
+      user: "the.alex.dev@ya.ru",
+      pass: "23NeLqnxBvLdPeR7dalW"
+    }
+  })
+  transporter.sendMail(
+    {
+      from: "the.alex.dev@ya.ru",
+      to: "the.alex.dev@ya.ru",
+      subject: "Заявка с Alex-Card.ru - Freelance",
+      html: `Имя: ${name} <br> Email: ${email} <br> Сообщение: ${message}`
+    },
+    (error, info) => {
+      if (error) {
+        return console.log(error)
+      }
+      console.log(info)
+    }
+  )
 }
 
-app.post('/', (req, res) => {
-	if (!(req && req.body.key == 'g1hXzgy2SOeWX2nSzQ8Z'))
-		return req.send(new Error('Invalid Key')).end()
+app.post("/", (req, res) => {
+  if (!(req && req.body.key == "g1hXzgy2SOeWX2nSzQ8Z"))
+    return req.send(new Error("Invalid Key")).end()
 
-	sendMail(req.body.name, req.body.email, req.body.message)
+  sendMail(req.body.name, req.body.email, req.body.message)
 
-	res.end()
+  res.end()
 })
 
 export default {
-	path: '/send-email',
-	handler: app
+  path: "/send-email",
+  handler: app
 }
 
 // import express from 'express'
